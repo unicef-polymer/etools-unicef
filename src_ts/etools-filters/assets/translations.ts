9b87658307cfb3636eb948a53322d8d90c41244a
Mercea Emil <office@insightfeatures.com>
2023-08-18 10:01:48 +0300

    Adding etools-filters

---
+++ src_ts/etools-filters/assets/translations.ts
@@ -0,0 +1,46 @@
+const translations = {
+  en: {
+    FILTERS: 'Filters',
+    CLEAR_ALL: 'Clear All',
+    SELECT: 'Select',
+    LOADING: 'Loading...'
+  },
+  fr: {
+    FILTERS: 'Filtres',
+    CLEAR_ALL: 'Tout effacer',
+    SELECT: 'Sélectionner',
+    LOADING: 'Chargement...'
+  },
+  pt: {
+    FILTERS: 'Filtros',
+    CLEAR_ALL: 'Limpar tudo',
+    SELECT: 'Selecionar',
+    LOADING: 'Carregando...'
+  },
+  ru: {
+    FILTERS: 'Фильтры',
+    CLEAR_ALL: 'Очистить все',
+    SELECT: 'Выбирать',
+    LOADING: 'Загрузка...'
+  },
+  ro: {
+    FILTERS: 'Filtre',
+    CLEAR_ALL: 'Elimină Tot',
+    SELECT: 'Selectează',
+    LOADING: 'Se încarcă...'
+  },
+  es: {
+    FILTERS: 'Filtros',
+    CLEAR_ALL: 'Limpiar todo',
+    SELECT: 'Seleccionar',
+    LOADING: 'Cargando...'
+  },
+  ar: {
+    FILTERS: 'المرشحات',
+    CLEAR_ALL: 'امسح الكل',
+    SELECT: 'يختار',
+    LOADING: 'جار التحميل...'
+  }
+};
+
+export default translations;
---
+++ src_ts/etools-filters/filters.ts
@@ -0,0 +1,83 @@
+import {AnyObject, RouteQueryParams} from '@unicef-polymer/etools-types';
+import {EtoolsFilter, EtoolsFilterTypes} from './etools-filters';
+
+export const isJsonStrMatch = (a: any, b: any) => {
+  return JSON.stringify(a) === JSON.stringify(b);
+};
+
+let selectedValueTypeByFilterKey: any;
+
+export function setselectedValueTypeByFilterKey(val: any) {
+  selectedValueTypeByFilterKey = val;
+}
+
+export const getSelectedFiltersFromUrlParams = (params: AnyObject) => {
+  const selectedFilters: AnyObject = {};
+
+  for (const filterKey in params) {
+    if (params[filterKey]) {
+      if (selectedValueTypeByFilterKey[filterKey] === 'Array' && !Array.isArray(params[filterKey])) {
+        selectedFilters[filterKey] = params[filterKey].split(',');
+      } else if (selectedValueTypeByFilterKey[filterKey] === 'boolean') {
+        selectedFilters[filterKey] = params[filterKey] === 'true';
+      } else {
+        selectedFilters[filterKey] = params[filterKey];
+      }
+    }
+  }
+  return selectedFilters;
+};
+
+export const updateFiltersSelectedValues = (params: RouteQueryParams, filters: EtoolsFilter[]) => {
+  const availableFilters = [...filters];
+
+  if (!Object.keys(params).length) {
+    clearSelectedValuesInFilters(filters);
+  }
+
+  const selectedFilters = getSelectedFiltersFromUrlParams(params);
+  for (const fKey in selectedFilters) {
+    if (fKey) {
+      const selectedValue = selectedFilters[fKey];
+      if (selectedValue) {
+        const filter = availableFilters.find((f: EtoolsFilter) => f.filterKey === fKey);
+        if (filter) {
+          filter.selectedValue = selectedValue instanceof Array ? [...selectedValue] : selectedValue;
+
+          filter.selected = true;
+        }
+      }
+    }
+  }
+
+  return availableFilters;
+};
+
+export function clearSelectedValuesInFilters(filters: EtoolsFilter[]) {
+  filters.forEach((f: EtoolsFilter) => {
+    f.selectedValue = getFilterEmptyValue(f.type);
+  });
+}
+
+export function getFilterEmptyValue(filterType: EtoolsFilterTypes) {
+  switch (filterType) {
+    case EtoolsFilterTypes.Search:
+      return '';
+    case EtoolsFilterTypes.Toggle:
+      return false;
+    case EtoolsFilterTypes.Date:
+    case EtoolsFilterTypes.Dropdown:
+      return null;
+    case EtoolsFilterTypes.DropdownMulti:
+      return [];
+  }
+}
+
+export const updateFilterSelectionOptions = (filters: EtoolsFilter[], fKey: string, options: AnyObject[]) => {
+  const filter = filters.find((f: EtoolsFilter) => f.filterKey === fKey);
+  if (filter && options) {
+    if (!isJsonStrMatch(filter.selectionOptions, options)) {
+      filter.selectionOptions = [...options];
+    }
+  }
+};
