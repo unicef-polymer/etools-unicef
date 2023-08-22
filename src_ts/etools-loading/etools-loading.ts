b6f978f1c8ad2b1600738786bdd09c5333fecdbb
merceaemil <30448376+merceaemil@users.noreply.github.com>
2023-08-22 12:36:01 +0300

    Added etools-loading and etools-profile-dropdown + support on vscode for shoelace elements (#23)

---
+++ src_ts/etools-loading/etools-loading.ts
@@ -0,0 +1,164 @@
+import {LitElement, html} from 'lit';
+import {property, customElement} from 'lit/decorators.js';
+import {getTranslation} from './utils/translate.js';
+import '@shoelace-style/shoelace/dist/components/spinner/spinner.js';
+/**
+ * `etools-loading`
+ *
+ * Loading spinner.
+
+ * You can use this loading element:
+ * - with an overlay: the loading spinner, message and overlay will be shown over your content area;
+ * - simple, no overlay: the loading spinner and the message will be displayed inline-block.
+ *
+ * ### Styling
+ *
+ * You can use defined variables for styling.
+ *
+ * Custom property | Description | Default
+ * ----------------|-------------|----------
+ * `--etools-loading-overlay-transparency` | Overlay transparency | `0.6`
+ * `--etools-loading-msg-color` | Loading message color | `#333333`
+ * `--etools-loading-spinner-size` | Spinner size (width & height) | `20px`
+ * `--etools-loading-bg-color` | Background color | `#ffffff`
+ * `--etools-loading-border-color` | Border color | `#dedede`
+ * `--etools-loading-shadow-color` | Shadow color | `#333333`
+ * `etools-loading::part(container)` | CSS Shadow Part applied to loading container | `{}`
+ * `etools-loading::part(message)` | CSS Shadow Part applied to loading message | `{}`
+
+ * To change spinner colors use sl-spinner styling variables([sl-spinner docs]
+ * (https://shoelace.style/components/spinner))
+ *
+ * @extends HTMLElement
+ * @polymer
+ * @customElement
+ * @demo demo/index.html
+ */
+@customElement('etools-loading')
+export class EtoolsLoading extends LitElement {
+  private _active: boolean;
+
+  @property({type: Boolean, reflect: true})
+  set active(val) {
+    this._active = val;
+    this._loadingStateChanged(val);
+    // reflect: true doesn't work in this case
+    if (val) {
+      this.setAttribute('active', '');
+    } else {
+      this.removeAttribute('active');
+    }
+  }
+
+  get active() {
+    return this._active;
+  }
+
+  @property({type: String, attribute: 'loading-text'})
+  loadingText: string;
+
+  @property({type: String, attribute: 'language'})
+  language!: string;
+
+  render() {
+    return html`
+      <style>
+        :host {
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          position: absolute;
+          top: 0;
+          right: 0;
+          bottom: 0;
+          left: 0;
+          background-color: rgba(180, 180, 180, var(--etools-loading-overlay-transparency, 0.6));
+          z-index: 50;
+          text-align: center;
+        }
+
+        :host([no-overlay]) {
+          background-color: transparent;
+          display: inline-block !important;
+          position: static;
+          width: auto;
+        }
+
+        :host(:not([active])) {
+          display: none !important;
+        }
+
+        .loading-message {
+          margin-inline-start: 15px;
+          color: var(--etools-loading-msg-color, #333333);
+        }
+
+        sl-spinner {
+          font-size: var(--etools-loading-spinner-size, 20px);
+          --track-width: var(--etools-loading-spinner-track-width, 3px);
+          --indicator-color: var(--etools-loading-spinner-indicator-color, #659cf7);
+          --track-color: var(--etools-loading-spinner-track-color, #dfdfdf);
+        }
+
+        :host(:not([no-overlay])) .loading-content {
+          background: var(--etools-loading-bg-color, #ffffff);
+          border: 1px solid var(--etools-loading-border-color, #dedede);
+          box-shadow: 0px 2px 5px -2px var(--etools-loading-shadow-color, #333333);
+          padding: 10px;
+          border-radius: 4px;
+        }
+
+        :host([absolute]) {
+          position: fixed;
+          z-index: 1000000;
+        }
+        .flex-h {
+          display: flex;
+          flex-direction: row;
+        }
+        .self-center {
+          align-self: center;
+        }
+      </style>
+      <div class="flex-h self-center">
+        <div class="flex-h self-center loading-content" part="container">
+          <sl-spinner></sl-spinner>
+          <span class="loading-message self-center" part="message">${this.loadingText}</span>
+        </div>
+      </div>
+    `;
+  }
+
+  constructor() {
+    super();
+    this._active = false;
+
+    if (!this.language) {
+      this.language = window.EtoolsLanguage || 'en';
+    }
+
+    this.loadingText = getTranslation(this.language, 'LOADING');
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    document.addEventListener('language-changed', this.handleLanguageChange.bind(this));
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    document.removeEventListener('language-changed', this.handleLanguageChange.bind(this));
+  }
+
+  handleLanguageChange(e) {
+    this.language = e.detail.language;
+  }
+
+  _loadingStateChanged(active) {
+    if (active) {
+      this.style.display = 'flex';
+    } else {
+      this.style.display = 'none';
+    }
+  }
+}
---
+++ src_ts/etools-profile-dropdown/etools-user-profile-dialog.ts
@@ -0,0 +1,329 @@
+import {LitElement, html} from 'lit';
+import {property, customElement} from 'lit/decorators.js';
+import '../etools-input/etools-input';
+import '../etools-dialog/etools-dialog';
+import '../etools-dropdown/etools-dropdown-multi';
+import '../etools-dropdown/etools-dropdown';
+import {getTranslation} from './utils/translate';
+import EtoolsDialog from '../etools-dialog/etools-dialog';
+
+/**
+ * @polymer
+ * @customElement
+ */
+@customElement('etools-user-profile-dialog')
+export class EtoolsUserProfileDialog extends LitElement {
+  private _profile: any = {};
+
+  @property({type: Object})
+  set profile(val) {
+    this._profile = val;
+    this._mapIds(this.profile);
+  }
+
+  get profile() {
+    return this._profile;
+  }
+
+  @property({type: Array})
+  offices: any = [];
+
+  @property({type: Array})
+  sections: any = [];
+
+  @property({type: Array})
+  users: any = [];
+
+  @property({type: Boolean})
+  readonly: boolean = true;
+
+  @property({type: Array})
+  availableCountryIds: any[] = [];
+
+  @property({type: Array})
+  availableGroups: any[] = [];
+
+  @property({type: Boolean})
+  showEmail: boolean = false;
+
+  @property({type: Boolean})
+  hideAvailableWorkspaces: boolean = false;
+
+  @property({type: String})
+  language: string = window.EtoolsLanguage || 'en';
+
+  render() {
+    // language=HTML
+    return html`
+      <style>
+        [hidden] {
+          display: none !important;
+        }
+
+        :host {
+          --paper-dialog-scrollable: {
+            width: 100%;
+            overflow-x: hidden;
+            max-height: 600px;
+          }
+        }
+
+        .paper-label {
+          font-size: 12px;
+          color: var(--secondary-text-color);
+          padding-top: 6px;
+        }
+
+        .input-label {
+          min-height: 24px;
+          padding-top: 4px;
+          padding-bottom: 6px;
+          min-width: 0;
+          font-size: 16px;
+        }
+
+        .input-label[empty]::after {
+          content: '—';
+          color: var(--secondary-text-color);
+        }
+
+        .separator {
+          padding: 0 8px;
+        }
+
+        etools-input {
+          width: 100%;
+        }
+
+        etools-input[readonly],
+        etools-dropdown-multi[readonly] {
+          pointer-events: none;
+          --etools-input-container-underline: {
+            display: none;
+          }
+        }
+
+        #profile-content {
+          overflow: hidden;
+          box-sizing: border-box;
+        }
+
+        .row-h {
+          display: flex;
+          flex-direction: row;
+        }
+
+        .flex-c {
+          /* flex container */
+          flex: 1;
+        }
+
+        .row-h + .row-h,
+        .row-v + .row-v {
+          margin-top: 20px;
+        }
+
+        .row-h:first-child + .row-v {
+          margin-top: 0;
+        }
+
+        .col {
+          display: flex;
+          flex-direction: row;
+          box-sizing: border-box;
+        }
+
+        .col:not(:first-child) {
+          padding-inline-start: 24px;
+        }
+
+        .col-6 {
+          flex: 0 0 50%;
+          max-width: 50%;
+        }
+
+        .col-12 {
+          flex: 0 0 100%;
+          max-width: 100%;
+        }
+
+        etools-dialog::part(ed-title) {
+          border-bottom: var(--epd-profile-dialog-border-b, none);
+        }
+        .flex-wrap {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .input-label.flex-wrap div {
+          padding-bottom: 6px;
+        }
+      </style>
+
+      <etools-dialog
+        id="userProfileDialog"
+        size="lg"
+        ok-btn-text="${getTranslation(this.language, 'OK_BTN_TEXT')}"
+        cancel-btn-text="${getTranslation(this.language, 'CANCEL_BTN_TEXT')}"
+        dialog-title="${getTranslation(this.language, 'MY_PROFILE')}"
+        ?hide-confirm-btn="${this.readonly}"
+        @close="${this._closeUserProfileDialog}"
+      >
+        <div id="profile-content" part="epd-user-profile-dropdown-content">
+          <!-- FIELDS HIDDEN AS REQUIRED BY BUSINESS SPECS - CH6215 -->
+          <!-- <div class="row-h flex-c">
+              <div class="col col-6">
+                <etools-dropdown id="office" label="Office" placeholder="—" .selected="${this.profile.office}"
+                                 .options="${this.offices}" auto-validate="" error-message="Please select an office">
+                </etools-dropdown>
+              </div>
+              <div class="col col-6">
+                <etools-dropdown id="section" label="Section" placeholder="—" .selected="${this.profile.section}"
+                                 .options="${this.sections}" auto-validate="" error-message="Please select a section">
+                </etools-dropdown>
+              </div>
+            </div>
+            <div class="row-h flex-c">
+              <div class="col col-6">
+                <etools-input label="Job title" .value="${this.profile.job_title}" placeholder="—"></etools-input>
+              </div>
+              <div class="col col-6">
+                <etools-input label="Phone number" .value="${this.profile.phone_number}" placeholder="—"></etools-input>
+              </div>
+            </div>
+            <div class="row-h flex-c">
+              <div class="col col-6">
+                <etools-input id="supervisor" label="My supervisor" placeholder="—" .value="${this.profile.supervisor}"
+                             readonly="">
+                </etools-input>
+              </div>
+              <div class="col col-6">
+                <etools-dropdown id="oic" label="My OIC" placeholder="—"
+                .selected="${this.profile.oic}" .options="${this.users}"
+                                 auto-validate="" error-message="Please select an OIC">
+                </etools-dropdown>
+              </div>
+            </div> -->
+          <div class="row-h flex-c">
+            <div class="col col-12">
+              <etools-input
+                id="name"
+                label="${getTranslation(this.language, 'NAME')}"
+                placeholder="&#8212;"
+                .value="${this.profile.name}"
+                readonly
+              ></etools-input>
+            </div>
+          </div>
+          <div class="row-h flex-c" ?hidden="${!this.showEmail}">
+            <div class="col col-12">
+              <etools-input
+                id="email"
+                label="${getTranslation(this.language, 'EMAIL')}"
+                placeholder="&#8212;"
+                .value="${this.profile.email}"
+                readonly
+              ></etools-input>
+            </div>
+          </div>
+          <div class="row-h flex-c" ?hidden="${this.hideAvailableWorkspaces}">
+            <div class="col col-12">
+              <div>
+                <label class="paper-label">${getTranslation(this.language, 'AVAILABLE_WORKSPACES')}</label>
+                <div class="input-label flex-wrap" ?empty="${this._emptyArray(this.profile.countries_available)}">
+                  ${(this.profile.countries_available || []).map(
+                    (item, index) => html`
+                      <div>
+                        ${item.name}
+                        <span class="separator">${this.getSeparator(this.profile.countries_available, index)}</span>
+                      </div>
+                    `
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row-h flex-c">
+            <div class="col col-12">
+              <div>
+                <label class="paper-label">${getTranslation(this.language, 'MY_GROUPS')}</label>
+                <div class="input-label flex-wrap" ?empty="${this._emptyArray(this.profile.groups)}">
+                  ${(this.profile.groups || []).map(
+                    (item, index) => html`
+                      <div>
+                        ${item.name}
+                        <span class="separator">${this.getSeparator(this.profile.groups, index)}</span>
+                      </div>
+                    `
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- <div class="row-h flex-c">
+              <div class="col col-12">
+                <etools-dropdown-multi id="supervisees" label="My supervisees" placeholder="—"
+                                        .selectedValues="${this.profile.supervisees}" .options="${this.users}" readonly>
+                </etools-dropdown-multi>
+              </div>
+            </div> -->
+        </div>
+      </etools-dialog>
+    `;
+  }
+
+  openUserProfileDialog() {
+    (this.shadowRoot!.querySelector('#userProfileDialog') as EtoolsDialog)!.opened = true;
+  }
+
+  _closeUserProfileDialog(e) {
+    if (e.detail.confirmed) {
+      this.saveData();
+    }
+  }
+
+  getSeparator(collection, index) {
+    if (!collection) {
+      return '';
+    }
+    if (index < collection.length - 1) {
+      return '|';
+    }
+    return '';
+  }
+
+  _mapIds(profile) {
+    if (profile === undefined) {
+      return;
+    }
+    let availableCountryIds;
+    let availableGroups;
+    if (!this.profile.countries_available) {
+      availableCountryIds = [];
+    } else {
+      availableCountryIds = this.profile.countries_available.map((x) => x['id']);
+    }
+
+    if (!this.profile.groups) {
+      availableGroups = [];
+    } else {
+      availableGroups = this.profile.groups.map((x) => x['id']);
+    }
+
+    this.availableCountryIds = availableCountryIds;
+    this.availableGroups = availableGroups;
+  }
+
+  _emptyArray(arr) {
+    return !arr || !arr.length;
+  }
+
+  saveData() {
+    this.dispatchEvent(
+      new CustomEvent('save-profile', {
+        detail: {profile: this.profile},
+        bubbles: true,
+        composed: true
+      })
+    );
+  }
+}
