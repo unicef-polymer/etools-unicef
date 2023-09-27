208f9a972bbc8967cfb554efdb7abb2dcc907042
Adriana Trif <trif.corina.adriana@gmail.com>
2023-09-27 13:01:28 +0300

    [ch35036Fix can't reselect an item after cancel (#47)

--- src_ts/etools-dropdown/SlAutocomplete.ts
+++ src_ts/etools-dropdown/SlAutocomplete.ts
@@ -625,6 +625,9 @@ export class SlAutocomplete extends LitElement {
     if (changedProperties.has('options') || changedProperties.has('selectedValues')) {
       const strSelectedVals = this.selectedValues ? this.selectedValues?.map((v) => String(v)) : this.selectedValues;
       this.selectedItems = this.options?.filter((o: any) => strSelectedVals?.includes(String(o[this.optionValue])));
+      if (this.selectedItems && !this.selectedItems.length) {
+        this.removePreventDeselectListeners();
+      }
     }
   }
 
