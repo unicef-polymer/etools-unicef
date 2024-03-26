a906bf5dfc988174be06ef1413b9730d65fc3939
Pendea Raul <raul.pendea@nordlogic.com>
2024-03-26 20:50:00 +0200

    fixed tooltip

--- src_ts/etools-info-tooltip/InfoIconTooltip.ts
+++ src_ts/etools-info-tooltip/InfoIconTooltip.ts
@@ -55,7 +55,7 @@ export class InfoIconTooltip extends LitElement {
         sl-tooltip::part(body) {
           box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
             0 3px 1px -2px rgba(0, 0, 0, 0.2);
-          pointer-events: unset;
+          pointer-events: all;
         }
         etools-icon:focus-visible {
           outline: 0;
